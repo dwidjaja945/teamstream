a72ae72a03c13419113c80f9142f53fad307eaef
Dylan Widjaja <dwidjaja945@gmail.com>
2018-05-07 16:36:43 -0700

    added more details to the dummyData for the athlete profiles

--- dummyData.js
+++ dummyData.js
@@ -3,28 +3,52 @@ var dummyData = [
         firstName: 'Dylan',
         lastName: 'Widjaja',
         age: 23,
-        height: '72',
-        weight: '84'
+        status: {
+            height: '72',
+            weight: '84'
+        },
+        id: 701,
+        photo: '',
+        role: '4 seat',
+        bio: "I'm Dylan and I row bro."
     },
     {
         firstName: 'Keith',
         lastName: 'Silcock',
         age: 27,
-        height: '78',
-        weight: '90'
+        status: {
+            height: '78',
+            weight: '90'
+        },
+        id: 702,
+        photo: '',
+        role: '3 seat',
+        bio: "I'm Keith. I don't row, but I certainly have the height and build for it. I just hope I won't get sunburned."
     },
     {
         firstName: 'Daniel',
         lastName: 'Donjuan',
         age: 56,
-        height: 'keiths height',
-        weight: '74'
+        status: {
+            height: 'keiths height',
+            weight: '74'
+        },
+        id: 703,
+        photo: '',
+        role: '1 seat',
+        bio: "I'm Daniel. I really should be a coxswain because I'm so small, but I wanna grow up to be just like Keith and row instead."
     },
     {
         firstName: "Christina",
         lastName: "So",
-        Age: 29,
-        height: '65'
-        weight: "61",
+        age: 29,
+        status: {
+            height: '65',
+            weight: "61"
+        },
+        id: 704,
+        photo: '',
+        role: '2 seat',
+        bio: "I'm Christina. I don't know why I'm here, I signed up to be on the women's team but ended up getting placed with the men's team. There must have been some kind of mixup."
     }
 ]

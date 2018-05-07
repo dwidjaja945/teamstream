c08184f7d7173f0f37423ba3305aab78b718674b
Dylan Widjaja <dwidjaja945@gmail.com>
2018-05-07 11:10:57 -0700

    created dummy data file

---
+++ dummyData.js
@@ -0,0 +1,30 @@
+var dummyData = [
+    {
+        firstName: 'Dylan',
+        lastName: 'Widjaja',
+        age: 23,
+        height: '72',
+        weight: '84'
+    },
+    {
+        firstName: 'Keith',
+        lastName: 'Silcock',
+        age: 27,
+        height: '78',
+        weight: '90'
+    },
+    {
+        firstName: 'Daniel',
+        lastName: 'Donjuan',
+        age: 56,
+        height: 'keiths height',
+        weight: '74'
+    },
+    {
+        firstName: "Christina",
+        lastName: "So",
+        Age: 29,
+        height: '65'
+        weight: "61",
+    }
+]

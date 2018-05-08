9996acb2a0ce9fb040eb0203b3e7ee58892ccc0d
KeithSilcock <silcockk@gmail.com>
2018-05-07 17:20:40 -0700

    added more bulletin board dummydata

--- dummyData_BB.js
+++ dummyData_BB.js
@@ -2,17 +2,56 @@ var dummyData_BB = [
     {
         firstName: 'Daniel',
         lastName: 'Donjuan',
-        postBody: 'When is practice?',
-        timeStamp: '09:30',
+        postBody:
+            {
+                bodyText: 'When is practice?',
+            },
+        timeStamp: '2016-06-19T07:00:00.000Z',
         photo: '',
         colorTag: 'green',
         pinned: false
     },
     {
+        firstName:'Keith',
+        lastName: 'Silcock',
+        postBody:{
+            bodyText: 'stuff and stuff and stuff all due tomorrow',
+        },
+        timeStamp:'1988-06-19T07:00:00.000Z',
+        photo:'',
+        colorTag:'blue',
+        pinned:false,
+    },
+    {
+        firstName:'Sam',
+        lastName: 'Skarvan',
+        postBody:{
+            bodyText: 'This is a wildly important post, but I don\'t have the permission to flag it as pinned',
+        },
+        timeStamp:'2018-03-14T14:02:00.000Z',
+        photo:'',
+        colorTag:'',
+        pinned:false,
+    },
+    {
+        firstName:'Dylan',
+        lastName: 'Widjaja',
+        postBody:{
+            bodyText: 'I\m the leader of this team and my words are law. I am a just, fair ruler',
+        },
+        timeStamp:'2015-05-24T15:30:00.000Z',
+        photo:'',
+        colorTag:'',
+        pinned:true,
+    },
+
+    {
         firstName: 'Dylan',
         lastName: 'Widjaja',
-        postBody: 'Hey guy, we\'re having practice tomorrow at 6pm at the boathouse.',
-        timeStamp: '13:00',
+        postBody: {
+            bodyText:'Hey guy, we\'re having practice tomorrow at 6pm at the boathouse.'
+        },
+        timeStamp: '2015-08-19T19:00:00.000Z',
         photo: '',
         colorTag: 'red',
         pinned: true
@@ -20,8 +59,10 @@ var dummyData_BB = [
     {
         firstname: 'Christina',
         lastName: 'So',
-        postBody: 'Team, I won\'t be able to make it to practice tomorrow. I have a thing',
-        timeStamp: '13:55',
+        postBody: {
+            bodyText:'Team, I won\'t be able to make it to practice tomorrow. I have a thing'
+        },
+        timeStamp: '2014-06-19T09:35:00.000Z',
         photo: '',
         colorTag: 'blue',
         pinned: false
@@ -29,8 +70,10 @@ var dummyData_BB = [
     {
         firstName: 'Keith',
         lastName: 'Silcock',
-        postBody: 'Looking forward to working out!',
-        timeStamp: '',
+        postBody: {
+            bodyText:'Looking forward to working out!'
+        },
+        timeStamp: '2008-09-19T10:00:00.000Z',
         photo: '',
         colorTag: 'green',
         pinned: false

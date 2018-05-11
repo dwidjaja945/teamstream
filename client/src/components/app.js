292380c24090dec50d2c3053af9366b8715e64e1
So-Xtina <mu7zic2soul@gmail.com>
2018-05-10 19:14:52 -0700

    created login and fork nav (join/create team)

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,12 +1,16 @@
 import React from "react";
 import AthleteProfile from "./athlete_profile";
-import Roster from './roster';
+import Roster from "./roster";
+import Login from "./login";
+import ForkNav from "./forkNav";
 
 function App() {
 	return (
 		<div className="container">
-			{/* <AthleteProfile /> */}
-			<Roster/>
+			{/* <Login /> */}
+			<ForkNav />
+			{/* <AthleteProfile />
+			<Roster /> */}
 		</div>
 	);
 }

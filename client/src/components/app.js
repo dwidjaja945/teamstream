5a33df5d8354380665624b4a6a9caa97f16ba4cf
ddonjuan <daniel.donjuan25@gmail.com>
2018-05-10 19:17:49 -0700

    saving my work

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,12 +1,14 @@
 import React from "react";
 import AthleteProfile from "./athlete_profile";
 import Roster from './roster';
+import Bulletin from './bulletin';
 
 function App() {
 	return (
 		<div className="container">
 			{/* <AthleteProfile /> */}
-			<Roster/>
+			{/* <Roster/> */}
+			<Bulletin/>
 		</div>
 	);
 }
---
+++ client/src/components/bulletin.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Bulletin extends Component{
+
+    render(){
+        return(
+            <div className="bulletinContainer">
+                <h1>This is going to be the bulletin section</h1>
+            </div>
+        )
+    }
+}
+export default Bulletin;

26af29e022fcb2fae9e8f8018663bef4164cdaa7
KeithSilcock <silcockk@gmail.com>
2018-05-21 11:08:50 -0700

    merge conflict

--- client/src/components/profile_pages/add_athlete.js
+++ client/src/components/profile_pages/add_athlete.js
@@ -11,7 +11,7 @@ class AddAthlete extends Component{
 
         this.addAthleteInput= this.addAthleteInput.bind(this);
     }
-    
+
     addAthleteInput(input){
         this.setState({
             profileData: {...this.state.profileData, ...input},

7b6094eb5f5fc9b1616edced4db05ad05f83cb24
KeithSilcock <silcockk@gmail.com>
2018-05-22 19:30:16 -0700

    added documentation to join_teams endpoint

--- client/src/components/create_team.js
+++ client/src/components/create_team.js
@@ -8,26 +8,32 @@ import ForkNav from "./fork_nav";
 class CreateTeam extends Component {
 	constructor(props) {
 		super(props);
+
+        this.state = {
+            team_name: "",
+
+        };
+
 	}
 
-	codeGenerator() {
-		let newCode = "";
-
-		for (let i = 0; i < 6; i++) {
-			let codeChoice = Math.floor(Math.random() * 2 + 1);
-			let code;
-
-			if (codeChoice === 1) {
-				let randomLetters = Math.floor(Math.random() * 26 + 65);
-
-				code = String.fromCharCode(randomLetters);
-			} else {
-				code = Math.floor(Math.random() * 9);
-			}
-			newCode += code;
-		}
-		return newCode;
-	}
+	// codeGenerator() {
+	// 	let newCode = "";
+    //
+	// 	for (let i = 0; i < 6; i++) {
+	// 		let codeChoice = Math.floor(Math.random() * 2 + 1);
+	// 		let code;
+    //
+	// 		if (codeChoice === 1) {
+	// 			let randomLetters = Math.floor(Math.random() * 26 + 65);
+    //
+	// 			code = String.fromCharCode(randomLetters);
+	// 		} else {
+	// 			code = Math.floor(Math.random() * 9);
+	// 		}
+	// 		newCode += code;
+	// 	}
+	// 	return newCode;
+	// }
 
 	render() {
 		return (
@@ -38,7 +44,7 @@ class CreateTeam extends Component {
 						<span className="teamName">Team Name</span>
 					</div>
 					<div className="cGNumberContainer">
-						<span className="cGNumber">{this.codeGenerator()}</span>
+						<span className="cGNumber">code gen here</span>
 					</div>
 					<Link to="/bulletin_board" className="cGbtnContent">
 						<span className="cGDoneBtn">Done</span>
@@ -50,3 +56,45 @@ class CreateTeam extends Component {
 }
 
 export default CreateTeam;
+
+
+class Bulletin extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            message: "new message"
+        };
+        this.handleAddMessage = this.handleAddMessage.bind(this);
+    }
+    handleAddMessage(event) {
+        event.preventDefault();
+
+        this.props.add(this.state.message);
+
+        this.setState({
+            message: ""
+        });
+    }
+    render() {
+        const { message } = this.state;
+        return (
+            <form onSubmit={this.handleAddMessage}>
+                <div className="inputContainer">
+                    <input
+                        value={message}
+                        type="text"
+                        placeholder="Enter Message"
+                        onChange={event => {
+                            this.setState({ message: event.target.value });
+                        }}
+                    />
+                </div>
+                <div className="buttonContainer">
+                    <button>Send</button>
+                </div>
+            </form>
+        );
+    }
+}
+export default Bulletin;

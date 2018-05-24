455cd7af51e7a73cd6a54f2b89e09c420a4833d3
So-Xtina <mu7zic2soul@gmail.com>
2018-05-24 10:42:11 -0700

    changed the home btn, logo center, and background of the navbar

--- client/src/components/roster/roster.js
+++ client/src/components/roster/roster.js
@@ -1,33 +1,28 @@
-import React, {Component} from "react";
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 import athleteRosterData from "./athlete_roster_data";
-import homeBtn from "../images/homeBtn.png";
+import homeBtn from "../images/team-stream-logo.png";
 import Navbar from "../navbar";
-import DisplayTeam from './displayTeam';
+import DisplayTeam from "./displayTeam";
 import "./roster.css";
 
-class Roster extends Component{
-	constructor(props){
+class Roster extends Component {
+	constructor(props) {
 		super(props);
 
 		this.state = {
 			teamData: []
-		}
-
-
+		};
 	}
-	componentWillMount(){
+	componentWillMount() {
 		this.getTeamData();
-
 	}
-	getTeamData(){
-
-
+	getTeamData() {
 		let path = "/api/roster";
 		axios.get(path).then(resp => {
 			console.log("Roster Response: ", resp);
 
-			const {athletes} = resp.data;
+			const { athletes } = resp.data;
 
 			this.setState({
 				teamData: resp.data.athletes
@@ -35,21 +30,16 @@ class Roster extends Component{
 		});
 	}
 
-	
-
-
-	render(){
-		const {teamData} = this.state;
-		console.log('Team roster data: ', this.state);
+	render() {
+		const { teamData } = this.state;
+		console.log("Team roster data: ", this.state);
 		return (
 			<div className="rosterContainer">
 				<Navbar icon={homeBtn} hamburgerMenu={true} url="/bulletin_board" />
 				<h1 className="rosterHeader">Roster</h1>
-				<DisplayTeam teamData={teamData}/>
+				<DisplayTeam teamData={teamData} />
 			</div>
 		);
-
 	}
 }
 export default Roster;
-

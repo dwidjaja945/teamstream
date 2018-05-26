6a875d3db468cdbdf2189a4278bd7fa9cdf4347b
Dylan Widjaja <dwidjaja945@gmail.com>
2018-05-26 12:23:02 -0700

    added endpoint to allow user to delete their own stats

--- routes/edit_athlete_stats.js
+++ routes/edit_athlete_stats.js
@@ -68,7 +68,6 @@ module.exports = ( webserver , dataBase , mysql ) => {
      * 
      * Returns:
      *   success: true
-     *   insertId
      */
     webserver.post( '/update_athlete_stats' , ( req , res ) => {
         const output = {
@@ -86,14 +85,15 @@ module.exports = ( webserver , dataBase , mysql ) => {
             return;
         }
 
-        let { stat_name, stat_value, athlete_id } = req.body;
+        let { stat_name, stat_value, athlete_id, stat_id } = req.body;
 
         let query = ` UPDATE stats
             SET stat_name = ? , stat_value = ?
             WHERE athlete_id = ?
+            AND stat_id = ?
             `;
 
-        let inserts = [ stat_name, stat_value, athlete_id ];
+        let inserts = [ stat_name, stat_value, athlete_id, stat_id ];
 
         let mysqlQuery = mysql.format( query , inserts );
 
@@ -108,5 +108,44 @@ module.exports = ( webserver , dataBase , mysql ) => {
 
             res.json(err);
         });
-    })
+    });
+
+    webserver.delete('/api/delete_athlete_stat', (req, res) => {
+        const output = {
+            success: false,
+            data: [],
+            errors: [],
+            redirect: ''
+        };
+
+        if (req.session.user_id === undefined) {
+            output.redirect = '/login';
+            output.errors = 'User not logged in';
+            res.json(output);
+            res.end();
+            return;
+        }
+
+        let { stat_id , athlete_id } = req.body;
+
+        let query = `DELETE FROM \`stats\` 
+        WHERE \`stat_id\` = ?
+        AND \`athlete_id\` = \`athlete_id\``;
+
+        let inserts = [ stat_id ];
+
+        let mysqlQuery = mysql.format(query, inserts);
+
+        dataBase.query(mysqlQuery, (err, data, fields) => {
+            if (!err) {
+                output.success = true;
+                output.data = data;
+                output.message = "Stat Deleted";
+                // output.redirect = '/athlete_profile'  // Might not need to redirect since will still be on edit page
+            } else {
+                output.errors = err;
+            }
+            res.json(data);
+        });
+    });
 };

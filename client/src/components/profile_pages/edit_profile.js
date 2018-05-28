2b4fb081642f8aa9684fe3a90941626144694a68
So-Xtina <mu7zic2soul@gmail.com>
2018-05-27 19:50:51 -0700

    completed toggle drop down menu

--- client/src/components/profile_pages/edit_profile.js
+++ client/src/components/profile_pages/edit_profile.js
@@ -1,158 +1,180 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import Field from './profile_fields';
-import AddNewInputs from './add_new_input';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Field from "./profile_fields";
+import AddNewInputs from "./add_new_input";
 import "../styles.css";
 import axios from "axios/index";
 
 class EditProfile extends Component {
-    constructor(props) {
-        super(props);
+	constructor(props) {
+		super(props);
 
-        this.state = {
-            first_name: '',
-            last_name: '',
-            age: '',
-            bio:'',
-            img_url:'',
-            height: '',
-            weight: '',
-            customStatsArray: [],
-        };
+		this.state = {
+			first_name: "",
+			last_name: "",
+			age: "",
+			bio: "",
+			img_url: "",
+			height: "",
+			weight: "",
+			customStatsArray: []
+		};
 
-        this.initialNumberOfStats=null;
-        this.pullAthleteProfileData();
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.addNewInput=this.addNewInput.bind(this);
-    }
+		this.initialNumberOfStats = null;
+		this.pullAthleteProfileData();
+		this.handleInputChange = this.handleInputChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.addNewInput = this.addNewInput.bind(this);
+	}
 
-    pullAthleteProfileData(){
-        let path = "/api/athlete_profile";
-        axios.get(`${path}`).then(response => {
-            if (response.data.success) {
-                console.log("data for athlete profile server response: ", response);
+	pullAthleteProfileData() {
+		let path = "/api/athlete_profile";
+		axios.get(`${path}`).then(response => {
+			if (response.data.success) {
+				console.log("data for athlete profile server response: ", response);
 
-                const users = response.data.user;
-                const userStatsArray=[];
-                for(let userIndex=0; userIndex<users.length; userIndex++){
-                    const {stat_id, stat_name, stat_value} = users[userIndex];
-                    userStatsArray.push({stat_id, stat_name, stat_value});
-                }
+				const users = response.data.user;
+				const userStatsArray = [];
+				for (let userIndex = 0; userIndex < users.length; userIndex++) {
+					const { stat_id, stat_name, stat_value } = users[userIndex];
+					userStatsArray.push({ stat_id, stat_name, stat_value });
+				}
 
-                this.setState({
-                    first_name: response.data.user[0].first_name,
-                    last_name: response.data.user[0].last_name,
-                    age: response.data.user[0].age,
-                    bio: response.data.user[0].bio,
-                    img_url: response.data.user[0].img_url,
-                    height: response.data.user[0].height,
-                    weight: response.data.user[0].weight,
-                    customStatsArray: userStatsArray,
-                });
+				this.setState({
+					first_name: response.data.user[0].first_name,
+					last_name: response.data.user[0].last_name,
+					age: response.data.user[0].age,
+					bio: response.data.user[0].bio,
+					img_url: response.data.user[0].img_url,
+					height: response.data.user[0].height,
+					weight: response.data.user[0].weight,
+					customStatsArray: userStatsArray
+				});
 
-                this.initialNumberOfStats = response.data.user.length;
-            } else {
-                //ERROR
-                console.log(response.data.errors);
-                this.props.history.push(response.data.redirect);
-            }
-        });
-    }
-    addNewInput(event) {
-        event.preventDefault();
+				this.initialNumberOfStats = response.data.user.length;
+			} else {
+				//ERROR
+				console.log(response.data.errors);
+				this.props.history.push(response.data.redirect);
+			}
+		});
+	}
+	addNewInput(event) {
+		event.preventDefault();
 
-        const newCustomInput={
-            inputName:'inputName',
-            stat_name:'',
-            valueName:'valueName',
-            stat_value:'',
-        };
+		const newCustomInput = {
+			inputName: "inputName",
+			stat_name: "",
+			valueName: "valueName",
+			stat_value: ""
+		};
 
-        this.setState({customStatsArray: [...this.state.customStatsArray, newCustomInput]});
-    }
+		this.setState({ customStatsArray: [...this.state.customStatsArray, newCustomInput] });
+	}
 
-    handleInputChange(event) {
-        const { value, name } = event.target;
+	handleInputChange(event) {
+		const { value, name } = event.target;
 
-        this.setState({
-            [name]: value,
-        });
-    }
+		this.setState({
+			[name]: value
+		});
+	}
 
-    addAthleteInput(){
-        let redirect = '/athlete_profile';
-        const {customStatsArray} = this.state;
-        const numOfStatsToUpdate = customStatsArray.length - this.initialNumberOfStats;
+	addAthleteInput() {
+		let redirect = "/athlete_profile";
+		const { customStatsArray } = this.state;
+		const numOfStatsToUpdate = customStatsArray.length - this.initialNumberOfStats;
 
-        console.log('updating stat and profile information')
-        //Axios call here for edited info and stats
+		console.log("updating stat and profile information");
+		//Axios call here for edited info and stats
 
+		let statsToUpdate = [];
+		//axios call for new stats
+		if (numOfStatsToUpdate > 0) {
+			console.log("Adding extra stats");
+			statsToUpdate = customStatsArray.slice(this.initialNumberOfStats);
 
-        let statsToUpdate = [];
-        //axios call for new stats
-        if(numOfStatsToUpdate > 0){
-            console.log('Adding extra stats')
-            statsToUpdate = customStatsArray.slice(this.initialNumberOfStats);
+			const path = "/api/add_athlete_stats";
+			axios.post(`${path}`, statsToUpdate).then(response => {
+				console.log("adding new stats response: ", response);
+				if (response.data.success) {
+					console.log("data for add-stat response: ", response);
+					const { insertStart, rowsAffected } = response.data.insertIds;
+					let count = 0;
+					for (let countIndex = insertStart; countIndex < rowsAffected + insertStart; countIndex++) {
+						statsToUpdate[count++].stat_id = countIndex;
+					}
+					redirect = response.data.redirect;
+				} else {
+					//ERROR
+					console.log(response.data.errors);
+				}
+			});
+		}
 
-            const path='/api/add_athlete_stats';
-            axios.post(`${path}`, statsToUpdate).then(response => {
-                console.log('adding new stats response: ', response)
-                if (response.data.success) {
-                    console.log("data for add-stat response: ", response);
-                    const {insertStart, rowsAffected} = response.data.insertIds;
-                    let count=0;
-                    for (let countIndex=insertStart; countIndex<rowsAffected+insertStart; countIndex++){
-                        statsToUpdate[count++].stat_id = countIndex;
-                    }
-                    redirect = response.data.redirect;
-                } else {
-                    //ERROR
-                    console.log(response.data.errors);
-                }
-            });
-        }
+		this.setState({
+			customStatsArray: [...this.state.customStatsArray, ...statsToUpdate]
+		});
+		this.props.history.push(redirect);
+	}
 
+	handleSubmit(event) {
+		event.preventDefault();
+		console.log("This is the handleSubmit: ", this.state);
 
-        this.setState({
-            customStatsArray: [...this.state.customStatsArray, ...statsToUpdate],
-        });
-        this.props.history.push(redirect);
-    }
+		this.addAthleteInput();
+	}
 
-    handleSubmit(event) {
-        event.preventDefault();
-        console.log('This is the handleSubmit: ', this.state);
-
-        this.addAthleteInput();
-    }
-
-    render() {
-        const { first_name, last_name, age, height, bio, img_url, weight, customStatsArray } = this.state;
-        console.log("edit profile current state: ", this.state)
-        return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <div>
-                        <h1>This is where user creates their profile</h1>
-                        <Field name="firstName" label="First Name" type="text" value={first_name} onChange={this.handleInputChange} />
-                        <Field name="lastName" label="Last Name" type="text" value={last_name} onChange={this.handleInputChange} />
-                        <Field name="age" label="Age" type="number" value={age} onChange={this.handleInputChange} />
-                        <Field name="height" label="Height" type="text" value={height} onChange={this.handleInputChange} />
-                        <Field name="weight" label="Weight" type="number" value={weight} onChange={this.handleInputChange} />
-                        <Field name="bio" label="Bio" type="text" value={bio} onChange={this.handleInputChange} />
-                        {/* <button onClick={this.addNewInput.bind(this)}>Add</button> */}
-                        <AddNewInputs addNewInput={this.addNewInput} customStatsArray={customStatsArray}/>
-                        {/* <Field name="statInput" type="text" value={statInput} onChange={this.handleInputChange} />
+	render() {
+		const { first_name, last_name, age, height, bio, img_url, weight, customStatsArray } = this.state;
+		console.log("edit profile current state: ", this.state);
+		return (
+			<div>
+				<form onSubmit={this.handleSubmit}>
+					<div>
+						<h1>This is where user creates their profile</h1>
+						<Field
+							name="firstName"
+							label="First Name"
+							type="text"
+							value={first_name}
+							onChange={this.handleInputChange}
+						/>
+						<Field
+							name="lastName"
+							label="Last Name"
+							type="text"
+							value={last_name}
+							onChange={this.handleInputChange}
+						/>
+						<Field name="age" label="Age" type="number" value={age} onChange={this.handleInputChange} />
+						<Field
+							name="height"
+							label="Height"
+							type="text"
+							value={height}
+							onChange={this.handleInputChange}
+						/>
+						<Field
+							name="weight"
+							label="Weight"
+							type="number"
+							value={weight}
+							onChange={this.handleInputChange}
+						/>
+						<Field name="bio" label="Bio" type="text" value={bio} onChange={this.handleInputChange} />
+						{/* <button onClick={this.addNewInput.bind(this)}>Add</button> */}
+						<AddNewInputs addNewInput={this.addNewInput} customStatsArray={customStatsArray} />
+						{/* <Field name="statInput" type="text" value={statInput} onChange={this.handleInputChange} />
                     <Field name="statValue" type="text" value={statValue} onChange={this.handleInputChange} /> */}
-                        <Link to={`/athlete_profile`} className="loginButtons">
-                            <span className="btnLog">Confirm Changes</span>
-                        </Link>
-                    </div>
-                    <button className="btnLog">Submit</button>
-                </form>
-            </div>
-        )
-    }
+						<Link to={`/athlete_profile`} className="loginButtons">
+							<span className="btnLog">Confirm Changes</span>
+						</Link>
+					</div>
+					<button className="btnLog">Submit</button>
+				</form>
+			</div>
+		);
+	}
 }
 export default EditProfile;
---
+++ client/src/components/toggle_team.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+import axios from "axios";
+import dropDown from "./images/double-down.png";
+
+class ToggleTeams extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			show: false,
+			teamCodes: []
+		};
+	}
+
+	componentDidMount() {
+		this.bulletinBoardAxios();
+	}
+
+	bulletinBoardAxios() {
+		const path = "/api/bulletin_board";
+
+		axios.get(path).then(response => {
+			console.log("BB info: ", response.data.userTeams);
+
+			const teamCodes = response.data.userTeams;
+
+			this.setState({
+				teamCodes: teamCodes
+			});
+		});
+	}
+
+	toggleAxios(id) {
+		const path = "/api/toggle_teams";
+
+		const dataToSend = { team_id: id };
+		axios.post(path, dataToSend).then(response => {
+			console.log("toggleAxios: ", response.data.userTeams);
+			this.props.refreshMessages();
+			this.setState({
+				show: false
+			});
+		});
+	}
+
+	render() {
+		const { show, teamCodes } = this.state;
+
+		const button = (
+			<div className="dropDownBtn" onClick={() => this.setState({ show: !show })}>
+				<img src={dropDown} />
+			</div>
+		);
+
+		const teams = teamCodes.map((code, index) => {
+			return (
+				<li
+					key={index}
+					onClick={() => {
+						this.toggleAxios(code.team_id);
+					}}
+				>
+					{code.team_name}
+				</li>
+			);
+		});
+
+		if (show) {
+			return (
+				<div className="toggleMenuBtn">
+					{button}
+					<ul className="toggleBullets">{teams}</ul>
+				</div>
+			);
+		}
+		return button;
+	}
+}
+
+export default ToggleTeams;

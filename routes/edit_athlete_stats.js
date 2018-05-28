d40da28b92469a10610072e56b8da055f4a4e8cd
Dylan Widjaja <dwidjaja945@gmail.com>
2018-05-28 15:13:14 -0700

    refactored the coe to edit the athlete information and athlete stats within a single axios call

--- routes/edit_athlete_stats.js
+++ routes/edit_athlete_stats.js
@@ -1,4 +1,119 @@
 module.exports = ( webserver , dataBase , mysql ) => {
+    
+    // ==============================
+    // ==== update athlete stats ====
+    // ==============================
+    function updateAthleteStats() {
+        return new Promise((resolve, reject) => {
+            const { athlete_id } = req.session;
+            let update_athlete_stats_query = `INSERT INTO stats
+                (
+                    stat_id
+                    stat_name,
+                    stat_value, 
+                    athlete_id
+                )
+                VALUES`;
+
+            let update_athlete_stats_inserts = [];
+
+            console.log('the request body: ', req.body)
+
+            // Inserting all potential stat entries into sql query
+            for (let statIndex = 0; statIndex < req.body.length; statIndex++) {
+                const { stat_name, stat_value } = req.body[statIndex];
+                let { stat_id } = req.body[statIndex ];
+
+                update_athlete_stats_query += " (?,?,?,?)";
+                update_athlete_stats_query += statIndex === req.body.length - 1 ? "" : ",";
+
+                // check to see if a stat_id has been provided or not.
+                if (isNaN(stat_id)) {
+                    stat_id = 'null';
+                };
+                update_athlete_stats_inserts.push(stat_id, stat_name, stat_value, athlete_id);
+            };
+
+            // Adding the ON DUPLICATE KEY UPDATE part of query
+            update_athlete_stats_query += `ON DUPLICATE KEY UPDATE 
+                stat_name = VALUES(stat_name),
+                stat_value = VALUES(stat_value)`;
+
+
+            let mysqlQuery = mysql.format(update_athlete_stats_query, update_athlete_stats_inserts);
+
+            dataBase.query(mysqlQuery, (err, data, fields) => {
+                if (!err) {
+                    output.insertIds = {
+                        insertStart: data.insertId,
+                        rowsAffected: data.affectedRows,
+                    }
+
+                    let deleteAthleteStatsQuery = `
+                        DELETE FROM stats 
+                        WHERE (stat_name, stat_value) 
+                        IN (('', ''))`;
+
+                    dataBase.query(deleteAthleteStatsQuery, (err, data, fields) => {
+                        if (!err) {
+                            output.updateAthleteStatsData = data;
+                            return resolve(output);
+                            // output.success = true;
+                            // output.redirect = "/athlete_profile";
+                        } else {
+                            output.updateAthleteStatsErrors = err;
+                            return reject(output);
+                        };
+                    })
+                    // output.data = data;
+                } else {
+                    output.errors = err;
+                    return reject(output);
+                }
+            });
+        });
+    };
+
+    // ==============================
+    // ==== update athlete info =====
+    // ==============================
+    function updateAthleteInfo() {
+        return new Promise((resolve, reject) => {
+            let { first_name, last_name, age, height, weight, bio, athlete_info_id } = req.body;
+
+            let update_athlete_info_query = `UPDATE athlete_info
+                SET first_name = ?,
+                    last_name = ?,
+                    age = ?,
+                    height = ?,
+                    weight = ?,
+                    bio = ?
+                WHERE athlete_info_id = ?`;
+
+            let update_athlete_info_inserts = [first_name, last_name, age, height, weight, bio, athlete_info_id];
+
+            for (let i = 0; i < update_athlete_info_inserts.length; i++) {
+                update_athlete_info_inserts[i] = slashes.add(update_athlete_info_inserts[i]);
+            }
+
+            let update_athlete_info_mysqlQuery = mysql.format(update_athlete_info_query, update_athlete_info_inserts);
+
+            dataBase.query(update_athlete_info_mysqlQuery, (err, data, fields) => {
+                if (!err) {
+                    output.updateAthleteInfoData = data
+                    return resolve(output);
+                } else {
+                    output.updateAthleteInfoErrors = err
+                    return reject(output);
+                };
+            });
+        })
+    };
+
+
+    // ==============================
+    // ==== Start Endpoints here ====
+    // ==============================
 
     /**
      * Takes : {
@@ -13,7 +128,6 @@ module.exports = ( webserver , dataBase , mysql ) => {
      */
     webserver.post( '/api/edit_athlete_stats' , ( req , res ) => {
         // console.log("edit athlete stats body: ", req.body)
-
         const output = {
             success: false,
             data: [],
@@ -22,77 +136,15 @@ module.exports = ( webserver , dataBase , mysql ) => {
         };
 
         if (req.session.user_id === undefined) {
-            output.redirect = '/login_page';
+            output.redirect = '/login';
             output.errors = 'User not logged in';
             res.json(output);
             res.end();
             return;
-        }
+        };
 
-        const {athlete_id} = req.session;
-        let query = `INSERT INTO stats
-            (
-                stat_id
-                stat_name,
-                stat_value, 
-                athlete_id
-            )
-            VALUES`;
-        let inserts = [];
+        Promise.all([ updateAthleteInfo() , updateAthleteStats() ] ).then( data => {res.json(data)} ).catch( errors => {res.json(errors)} );
 
-        console.log('the request body: ', req.body)
-
-        for(let statIndex=0; statIndex<req.body.length; statIndex++){
-            const {stat_name, stat_value} = req.body[statIndex];
-            let { stat_id } = req.body[ statIndex ];
-
-            query += ' (?,?,?,?)';
-            query += statIndex===req.body.length-1 ? '' : ',';
-
-            // check to see if a stat_id has been provided or not.
-            if( isNaN( stat_id ) ) {
-                stat_id = 'null';
-            };
-
-            inserts.push(stat_id, stat_name, stat_value, athlete_id);
-        }
-
-        query += `ON DUPLICATE KEY UPDATE 
-            stat_name = VALUES(stat_name),
-            stat_value = VALUES(stat_value)`;
-
-
-        let mysqlQuery = mysql.format( query , inserts );
-
-        dataBase.query( mysqlQuery , ( err , data , fields ) => {
-            if(!err) {
-                output.insertIds = {
-                    insertStart: data.insertId,
-                    rowsAffected: data.affectedRows,
-                }
-
-                let query = `
-                        DELETE FROM stats 
-                        WHERE (stat_name, stat_value) 
-                        IN (('', ''))`;
-
-                dataBase.query(query, (err, data, fields) => {
-                    if (!err) {
-                        output.success = true;
-                        output.redirect = "/athlete_profile";
-                    } else {
-                        output.errors = err;
-                    };
-                    console.log('edit athlete output: ', output);
-                    res.json(output);
-                })
-                // output.data = data;
-            } else {
-                output.errors = err;
-                console.log('edit athlete output error: ' , output);
-                res.json(output);
-            }
-        });
     });
 
     webserver.delete('/api/delete_athlete_stat', (req, res) => {

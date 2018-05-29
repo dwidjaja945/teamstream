40eccabc81dc853850f59da38babf7ab2f905f38
So-Xtina <mu7zic2soul@gmail.com>
2018-05-28 19:56:08 -0700

    added additional styling to each of the pages...

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,25 @@
-module.exports = function (webserver, dataBase, mysql) {
-    require("./create_team")(webserver, dataBase, mysql);
+module.exports = function(webserver, dataBase, mysql) {
+	require("./create_team")(webserver, dataBase, mysql);
 
-    require("./join_team")(webserver, dataBase, mysql);
+	require("./join_team")(webserver, dataBase, mysql);
 
-    require("./create_athlete_info")(webserver, dataBase, mysql);
+	require("./create_athlete_info")(webserver, dataBase, mysql);
 
-    require('./edit_athlete_stats')(webserver, dataBase, mysql);
+	require("./edit_athlete_stats")(webserver, dataBase, mysql);
 
-    // require("./update_athlete_info")(webserver, dataBase, mysql);
+	// require("./update_athlete_info")(webserver, dataBase, mysql);
 
-    require('./toggle_teams')( webserver , dataBase , mysql );
+	require("./toggle_teams")(webserver, dataBase, mysql);
 
-    // require("./athlete_profile")(webserver, dataBase, mysql);
+	// require("./athlete_profile")(webserver, dataBase, mysql);
 
-    // endpoint for roster
-    require("./roster")(webserver, dataBase, mysql);
+	// endpoint for roster
+	require("./roster")(webserver, dataBase, mysql);
 
-    // endpoint for bulletin board
-    require("./bulletin_board")(webserver, dataBase, mysql);
+	// endpoint for bulletin board
+	require("./bulletin_board")(webserver, dataBase, mysql);
 
-    require("./pinned")(webserver, dataBase, mysql);
+	require("./pinned")(webserver, dataBase, mysql);
 
-    require("./logout")(webserver, dataBase, mysql);
-}
-
-
+	require("./logout")(webserver, dataBase, mysql);
+};

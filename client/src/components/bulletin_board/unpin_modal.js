ca464e4bee7697007c61352c0b0c559dda151213
KeithSilcock <silcockk@gmail.com>
2018-05-30 18:45:07 -0700

    changed functionality so that bulliten board doesn't prompt a warning if there is no pinned message changed zindex of modal to be above user images

--- client/src/components/bulletin_board/unpin_modal.js
+++ client/src/components/bulletin_board/unpin_modal.js
@@ -6,29 +6,29 @@ class UnpinModal extends Component{
     }
 
     render(){
+        const { post_id, pin_level, pinMessage, openModal, modalCallBack, hasPinned } = this.props;
         const modalClass = this.props.openModal ? 'modal show-modal' : 'modal';
-        const { post_id, pin_level, pinMessage, openModal, modalCallBack } = this.props;
 
         return(
-            <div className={modalClass}>
-                <div className="modal-content">
+        <div className={modalClass}>
+            <div className="modal-content">
                     <span className="close-button" onClick= {()=>{
-                                               modalCallBack();
+                        modalCallBack();
 
                     }}>&times;</span>
-                    <h2 className="modal-header">Warning:</h2>
-                    <h4 className="modal-body">Are you sure you want to unpin the current pinned post? </h4>
-                    <button onClick={ () => {
-                        pinMessage(post_id, pin_level);
-                       modalCallBack();
-                    } }
-                    >Confirm</button>
-                    <button className="cancel" onClick={()=>{
-                       modalCallBack();
-                    }}>Cancel</button>
-                </div>
+                <h2 className="modal-header">Warning:</h2>
+                <h4 className="modal-body">Are you sure you want to unpin the current pinned post? </h4>
+                <button onClick={ () => {
+                    pinMessage(post_id, pin_level);
+                    modalCallBack();
+                } }
+                >Confirm</button>
+                <button className="cancel" onClick={()=>{
+                    modalCallBack();
+                }}>Cancel</button>
             </div>
-        )   
+        </div>
+    )
     }
 }
 export default UnpinModal;

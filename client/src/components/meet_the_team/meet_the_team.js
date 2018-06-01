97cc7139e66cf2299c6f4074ec94b7dc6d1c1157
ddonjuan <daniel.donjuan25@gmail.com>
2018-06-01 10:33:22 -0700

    finsihed about page

--- client/src/components/meet_the_team/meet_the_team.js
+++ client/src/components/meet_the_team/meet_the_team.js
@@ -4,6 +4,8 @@ import NavBar from "../navbar";
 import hamburgerMenu from "../hamburger_menu";
 import homeBtn from "../images/team-stream-logo.png";
 import meetTheTeamData from './meet_the_team_data';
+import gitHubLogo from '../images/github.png';
+import teamLogo from '../images/team_stream_logo3x.png';
 import './meet_team.css';
 
 
@@ -17,12 +19,15 @@ class MeetTheTeam extends Component {
     render() {
         const team = meetTheTeamData.map((item, index) => {
             return (
-                <div className='teamDev'>
+                <div className='teamDev' key={index}>
                     <img className="teamPic" src={item.pic} alt="batman" />
                     <div className="teamBody">
-                        <h2>{item.title}</h2>
-                        <h3>{item.firstName} {item.lastName}</h3>
-                        <p>{item.bio}</p>
+                        <h3>{item.title}</h3>
+                        <h4>{item.firstName} {item.lastName}</h4>
+                        <p>{item.position}</p>
+                        <p>{item.quote}</p>
+                        <a href={item.bio} target="_blank"><img className="github" src={gitHubLogo} alt="github Logo"/></a>
+                        
                     </div>
 
 
@@ -35,6 +40,7 @@ class MeetTheTeam extends Component {
                 <NavBar icon={homeBtn} hamburgerMenu={true} url="/bulletin_board" />
                 <div className="meetTeam">
                 <h1>Team TeamStream</h1>
+                <img  className="teamLogo" src={teamLogo} alt=""/>
                     {team}
                 </div>
             </div>

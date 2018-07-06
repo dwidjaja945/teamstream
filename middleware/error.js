4b3e7f1914fc40065ec638277767af666fc671e9
Dylan Widjaja <dwidjaja945@gmail.com>
2018-07-06 16:55:44 -0700

    fixed a typo

--- middleware/error.js
+++ middleware/error.js
@@ -4,5 +4,5 @@ module.exports = function (err, req, res, next) {
     let date = new Date;
     err.date = date.toLocaleString();
     winston.error(err.message, err);
-    res.status(500).send("Error occured");
+    res.status(500).send("Error occurred");
 }
